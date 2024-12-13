e868a6471e82960b6760e71dd5e3d4a7a143e8f2
suggulokesh27 <suggulokesh27@gmail.com>
2024-12-13 13:27:57 +0530

    change the generate static params

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['tailwindui.com',"via.placeholder.com","assets.ajio.com","www.ajio.com"],
+    domains: [
+      'tailwindui.com',
+      "via.placeholder.com",
+      "assets.ajio.com",
+      "www.ajio.com"
+    ],
+    unoptimized: true, // Disable image optimization
   },
+  output: "export", // Enable static export
+  reactStrictMode: true,
 };
 
 export default nextConfig;
